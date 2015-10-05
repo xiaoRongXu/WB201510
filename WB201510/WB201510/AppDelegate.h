bf7e7fa6bf7a446fb315bedaf0ecbd460daedf8d
wangbo <developforapple@163.com>
2015-10-05 12:36:33 +0800

    Create
    Create project

---
+++ WB201510/WB201510/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WB201510
+//
+//  Created by wwwbbat on 15/10/5.
+//  Copyright © 2015年 wwwbbat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WB201510/WB201510/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WB201510
+//
+//  Created by wwwbbat on 15/10/5.
+//  Copyright © 2015年 wwwbbat. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
